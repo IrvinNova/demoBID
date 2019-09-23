464a8e0a60dea68cddbd06932b9e7567f6927966
Arturo Serratos <42125090+ronnygtas@users.noreply.github.com>
2019-09-22 19:15:44 -0500

    Creados modulos de contracts

---
+++ src/app/componets/user/user-documents/user-documents.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-documents',
+  templateUrl: './user-documents.component.html',
+  styleUrls: ['./user-documents.component.scss'],
+})
+export class UserDocumentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
---
+++ src/app/componets/user/user-finger-sign/user-finger-sign.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-finger-sign',
+  templateUrl: './user-finger-sign.component.html',
+  styleUrls: ['./user-finger-sign.component.scss'],
+})
+export class UserFingerSignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
