891931f2d9404f9b822a6af9d618d4d812266256
Arturo Serratos <42125090+ronnygtas@users.noreply.github.com>
2019-09-23 19:13:02 -0500

    Console Finger

--- src/app/componets/user/user-enroll/user-enroll.component.ts
+++ src/app/componets/user/user-enroll/user-enroll.component.ts
@@ -90,6 +90,7 @@ export class UserEnrollComponent implements OnInit {
   public enroll() {
     this.loading.show();
     this.storage.save(environment.hand, this.data.data);
+    console.log('Finger antes de Enroll', this.data.data);
     // Servicio de enrolamiento de huellas del cliente
     this.biometric_serv.enrollBiometric(this.data.data, this.token, this.operation.operationId, this.operation.systemCode, this.person.id, '', 2)
     .subscribe( result => {
